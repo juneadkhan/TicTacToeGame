9315d440c4e6702fb8f316e114da200a4af5549f
JuneadKhan <junead01@googlemail.com>
2020-08-21 22:26:18 +0100

    Added Default values into NoughtBoard_UI

---
+++ src/tic_tac_toe/NoughtBoard.java
@@ -0,0 +1,12 @@
+package tic_tac_toe;
+
+public interface NoughtBoard {
+	
+	int getWidth();
+	 
+	int getHeight();
+
+	Nought getAt(int x, int y);
+
+
+}
--- src/tic_tac_toe/NoughtBoard_UI.java
+++ src/tic_tac_toe/NoughtBoard_UI.java
@@ -1,5 +1,43 @@
 package tic_tac_toe;
 
-public class NoughtBoard_UI {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+public class NoughtBoard_UI extends JPanel implements NoughtBoard {
+
+	// Set Constants
+	private static final int DEFAULT_SCREEN_WIDTH = 500;
+	private static final int DEFAULT_SCREEN_HEIGHT = 500;
+	private static final Color DEFAULT_BACKGROUND_LIGHT = new Color(0.8f, 0.8f, 0.8f);
+	private static final Color DEFAULT_BACKGROUND_DARK = new Color(0.5f, 0.5f, 0.5f);
+
+	private static final Color DEFAULT_SPOT_COLOR = Color.BLACK;
+	private static final Color DEFAULT_HIGHLIGHT_COLOR = Color.YELLOW;
+
+	// Effectively a matrix of positions for the naughts. 
+	// The 2D-Array acts as the board.
+	private Nought[][] _board;
+
+	//Getters
+	public int getWidth() {
+
+		return _board.length;
+	}
+
+	public int getHeight() {
+
+		return _board[0].length;
+	}
+
+	public Nought getAt(int x, int y) {
+
+		return _board[x][y];
+	}
 
 }
