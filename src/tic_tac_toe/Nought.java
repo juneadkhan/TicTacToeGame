dd7d93fd5f899a718a9b0806bd29120afc2396cc
JuneadKhan <junead01@googlemail.com>
2020-08-21 20:26:52 +0100

    Added Nought Interface and UI_Element class. Added Coordinate Fields.

---
+++ src/tic_tac_toe/Nought.java
@@ -0,0 +1,12 @@
+package tic_tac_toe;
+
+//A Nought represents an individual piece
+public interface Nought {
+
+	//Returns X/Y Coordinates
+	int getX();
+	int getY();
+	
+	
+	
+}
---
+++ src/tic_tac_toe/Nought_UI_Element.java
@@ -0,0 +1,23 @@
+package tic_tac_toe;
+
+public class Nought_UI_Element implements Nought {
+
+	//Coordinates for Nought_UI_Element Object
+	private int _x;
+	private int _y;
+	
+	//Getter for X Component
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return _x;
+	}
+
+	//Getter for Y Component
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return _y;
+	}
+
+}
