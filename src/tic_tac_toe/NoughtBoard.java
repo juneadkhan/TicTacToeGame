1c175116aa1d6880970b590f29c35783f74aeaad
JuneadKhan <junead01@googlemail.com>
2020-08-22 13:39:04 +0100

    Updated NoughtBoard Interface for Iterator

--- src/tic_tac_toe/NoughtBoard.java
+++ src/tic_tac_toe/NoughtBoard.java
@@ -1,12 +1,23 @@
 package tic_tac_toe;
 
-public interface NoughtBoard {
+import java.util.Iterator;
+
+public interface NoughtBoard extends Iterable<Nought> {
 	
 	int getWidth();
 	 
 	int getHeight();
 
 	Nought getAt(int x, int y);
+	
+	void addListener(Listener listener);
+	
+	void removeListener(Listener listener);
+	
+	Iterator<Nought> iterator();
+
+
+
 
 
 }
