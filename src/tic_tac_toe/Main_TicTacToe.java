b6dc18a66a27b6a0edb7e69a6aadf03373938b53
juneadkhan <junead@live.unc.edu>
2020-08-22 14:02:02 +0100

    Created main UI Frame

--- src/tic_tac_toe/Main_TicTacToe.java
+++ src/tic_tac_toe/Main_TicTacToe.java
@@ -1,8 +1,29 @@
 package tic_tac_toe;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 //Author: Junead Khan
 
 public class Main_TicTacToe {;
+	public static void main(String[] args) {
+		
+		//Creates the main UI Window for the program to run
+		JFrame mainWindow_UI = new JFrame();
+		mainWindow_UI.setTitle("TicTacToe");
+		mainWindow_UI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+		//Creates the main panel for content
+		JPanel topPanel = new JPanel();
+		topPanel.setLayout(new BorderLayout());
+		mainWindow_UI.setContentPane(topPanel);
 
+	
+		//This packs the main frame and sets it to be visible
+		mainWindow_UI.pack();
+		mainWindow_UI.setVisible(true);		
+	}
 
 }
